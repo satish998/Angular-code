a7fe8326b1feacf7406ec823aa92771ac7d9f2ad
satish kumar <100787234+satish998@users.noreply.github.com>
2025-07-05 02:49:33 +0000

    applying global style

--- anular19-app/src/app/app.component.ts
+++ anular19-app/src/app/app.component.ts
@@ -3,10 +3,18 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'anular19-app';
+
+  printValue(val: string) {
+    console.log('Input value:', val);
+  }
+
+  show(value: string) {
+  console.log('Value:', value);
+}
 }
